f320408724e5b30d6a7994567299e86479284370
Sydney Pogue <spogue1441@gmail.com>
2023-06-14 05:13:10 -1000

    added two new fields to user model: about-me and and websitelink

--- models/user.js
+++ models/user.js
@@ -1,20 +1,16 @@
 'use strict';
 const bcrypt = require('bcryptjs');
-
 const {
   Model
 } = require('sequelize');
+
 module.exports = (sequelize, DataTypes) => {
   class user extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
     static associate(models) {
 
     }
   }
+
   user.init({
     name: {
       type: DataTypes.STRING,
@@ -41,30 +37,33 @@ module.exports = (sequelize, DataTypes) => {
           msg: "Password must be between 8 and 99 characters"
         }
       }
+    },
+    aboutMe: { // added field
+      type: DataTypes.TEXT,
+      allowNull: true
+    },
+    website: { // added field
+      type: DataTypes.STRING,
+      allowNull: true
     }
   }, {
     sequelize,
     modelName: "user",
   });
-  // Before a user is created, we are encrypting the password and using hash in its place
-  user.addHook('beforeCreate', (pendingUser) => { // pendingUser is user object that gets passed to DB
-    // Bcrypt is going to hash the password
-    let hash = bcrypt.hashSync(pendingUser.password, 12); // hash 12 times
-    pendingUser.password = hash; // this will go to the DB
+
+  user.addHook('beforeCreate', (pendingUser) => {
+    let hash = bcrypt.hashSync(pendingUser.password, 12);
+    pendingUser.password = hash;
   });
 
-  // Check the password on Sign-In and compare it to the hashed password in the DB
   user.prototype.validPassword = function (typedPassword) {
-    let isCorrectPassword = bcrypt.compareSync(typedPassword, this.password); // check to see if password is correct.
-
+    let isCorrectPassword = bcrypt.compareSync(typedPassword, this.password);
     return isCorrectPassword;
   }
 
-  // return an object from the database of the user without the encrypted password
   user.prototype.toJSON = function () {
     let userData = this.get();
-    delete userData.password; // it doesn't delete password from database, only removes it. 
-
+    delete userData.password;
     return userData;
   }
 
